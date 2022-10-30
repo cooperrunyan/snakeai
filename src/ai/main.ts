b81edf84d2c209e1160ca9e7fa4df052630c931c
cooperrunyan <cooperrunyanspam@gmail.com>
2022-10-30 11:40:52 -0700

    Move files to src/ai

--- src/ai/main.ts
+++ src/ai/main.ts
@@ -6,27 +6,19 @@ import { move } from './lib/move';
 import { onTick } from './lib/onTick';
 import { panic } from './panic';
 import { config } from './state/config';
-// import { Apple } from './types/Apple';
-// import { Cell } from './types/Cell';
 import { Direction } from './types/Direction';
 
 init(false);
 
-// let lastApple: Apple = [-1, -1];
-// let route: Cell[] = [];
-
-onTick((snake, apple, direction /*died*/) => {
+onTick((snake, apple, direction) => {
   try {
     const head = snake.at(-1)!;
 
     if (boardAvailable(snake.slice()).percent <= 0.8) return panic(snake, direction);
 
-    // if (route.at(-1) && !died && lastApple[0] + ' ' + lastApple[1] === apple[0] + ' ' + apple[1] && !route.at(-1)?.wall) route = route.slice(0, -1);
-    // else route = closest(snake.slice(), apple, direction) || [];
-
     const route = closest(snake.slice(), apple, direction);
 
-    if (!route /*[0]*/) return panic(snake, direction);
+    if (!route) return panic(snake, direction);
 
     const nextHead = route.at(-2)!;
 
@@ -41,8 +33,6 @@ onTick((snake, apple, direction /*died*/) => {
     } else if (nextHead.y < head[2]) {
       if (head[2] - 1 >= 0) move(Direction.Up);
     }
-
-    // lastApple = apple;
   } catch {}
 });
 
